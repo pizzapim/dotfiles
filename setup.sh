b92959aefbf65f1571ec76da64f10ccf069edaa7
Pim Kunis <pim@kunis.nl>
2020-12-19 22:13:59 +0100

    regolith changes

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+
 link () {
 	mkdir -p -- "$2"
 	ln --symbolic --force "$PWD"/"$1" "$2"
 }
 
-link i3 $HOME/.config
-link compton.conf $HOME/.config
-link .spacemacs $HOME
+#link i3 $HOME/.config
+#link compton.conf $HOME/.config
+#link .spacemacs $HOME
 link .gitconfig $HOME
-link .bashrc $HOME
+#link .bashrc $HOME
 link .bash_aliases $HOME
-link nvim $HOME/.config
-link fish $HOME/.config
-link .zshrc $HOME
+#link nvim $HOME/.config
+#link fish $HOME/.config
+#link .zshrc $HOME
 link settings.json $HOME/.config/VSCodium/User
+link regolith/Xresources $HOME/.config/regolith
